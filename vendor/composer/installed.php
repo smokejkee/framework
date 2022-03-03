e2197c9e70fe99725d6f803a433d7de88b20f35c
SmokeJkee <niikitos2012@gmail.com>
2022-03-03 04:06:40 +0300

    fix

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'd30507c3628d73de28bf65881ffb01d339255b6b',
         'name' => 'smokejkee/test2',
         'dev' => true,
     ),
     'versions' => array(
         'smokejkee/test2' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'd30507c3628d73de28bf65881ffb01d339255b6b',
             'dev_requirement' => false,
         ),
     ),
